1e25cc63fe7c073bb7bb72571ffa1cf6002f7da6
Ismail Khan <ismail.khan02@gmail.com>
2026-03-04 12:33:15 +0000

    Added fix using depends_on

--- terraform_configs/main.tf
+++ terraform_configs/main.tf
@@ -1,6 +1,7 @@
 # Configure the Kubernetes provider to use the EKS cluster
 data "aws_eks_cluster" "eks_cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks.cluster_name]
 }
 
 # Get authentication token for the EKS cluster
@@ -9,9 +10,8 @@ data "aws_eks_cluster_auth" "eks_cluster" {
 }
 # Configure the Kubernetes provider to use the EKS cluster
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
 }
 
 
